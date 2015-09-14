2695690b895475da85c9c38044b7b20d87e1ef3d
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-09-14 23:59:49 +0200

    Tried to make it async

--- GWvW_Overlay-Location-Server-Contracts/ILocationServiceCallBack.cs
+++ GWvW_Overlay-Location-Server-Contracts/ILocationServiceCallBack.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ServiceModel;
+using System.Threading.Tasks;
 
 namespace GWvW_Overlay_Location_Server_Contracts
 {
     public interface ILocationServiceCallBack
     {
-        [OperationContract(IsOneWay = true)]
-        void ReceivePositions(List<Position> positions);
+        [OperationContract()]
+        Task ReceivePositions(List<Position> positions);
     }
 }
